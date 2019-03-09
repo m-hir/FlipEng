9435b47a49931359622416f968b01e2a49fc978e
m.hirata <momoko.hirata.1109@gmail.com>
2019-03-09 12:45:19 +0900

    add FlipCard仮実装

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,55 @@
 import ReactDOM from 'react-dom'
 import * as React from 'react';
+import * as img1 from './img_card_front.png';
 
-class MyComponent extends React.Component {
+interface Props {
+    title:string;
+    speed:number;
+}
+class FlipCard extends React.Component<Props> {
+    static defaultProps:Props = {
+        title:"",
+        speed:0
+    }
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+    private onClick() {
+        this.props.onClick();
+    }
     render () {
-        return (<h1>おはよ</h1>);
+        return (
+            <div className="card" onClick={this.onClick.bind(this)}>
+                <p>{this.props.test}</p>
+                {/* <h1>おはよ</h1> */}
+                <div className="card-container">
+                    <div className="card-item card-front">
+                        <img src={img1.default} alt="" />
+                    </div>
+                    <div className="card-item card-back">
+                        <img src="img_card_back.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (<div>
+            <h1>おはよー</h1>
+            <FlipCard title="さんぷる１" speed={0} onClick={() => {  console.log("hoge"); }} />
+            <FlipCard test="ok"/>
+            <FlipCard />
+        </div>)
     }
 }
 
 
 ReactDOM.render(
-    <MyComponent />, 
+    <App />, 
     document.getElementById("app")
 );
