fdc433dc706119bb08df99a52d9eaab50926fdee
m.hirata <momoko.hirata.1109@gmail.com>
2019-03-09 16:56:11 +0900

    update フリップ機能実装

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import * as React from 'react';
 import * as img1 from './img_card_front.png';
+import * as img2 from './img_card_back.png';
 
 interface Props {
     title:string;
@@ -21,15 +22,17 @@ class FlipCard extends React.Component<Props> {
     }
     render () {
         return (
-            <div className="card" onClick={this.onClick.bind(this)}>
-                <p>{this.props.test}</p>
+            <div className="card">
+                {/* <p>{this.props.test}</p> */}
                 {/* <h1>おはよ</h1> */}
                 <div className="card-container">
                     <div className="card-item card-front">
-                        <img src={img1.default} alt="" />
+                        {/* <img src={img1.default} alt="" /> */}
+                        <div className="text">by the way</div>
                     </div>
                     <div className="card-item card-back">
-                        <img src="img_card_back.png" alt="" />
+                        {/* <img src={img2.default} alt="" /> */}
+                        <div className="text">ところで</div>
                     </div>
                 </div>
             </div>
@@ -40,9 +43,9 @@ class FlipCard extends React.Component<Props> {
 class App extends React.Component {
     render() {
         return (<div>
-            <h1>おはよー</h1>
-            <FlipCard title="さんぷる１" speed={0} onClick={() => {  console.log("hoge"); }} />
-            <FlipCard test="ok"/>
+            {/* <h1>おはよー</h1> */}
+            {/* <FlipCard title="さんぷる１" speed={0} onClick={() => {  console.log("hoge"); }} />
+            <FlipCard test="ok"/> */}
             <FlipCard />
         </div>)
     }
